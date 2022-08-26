b03ee35431b7a968c3079eca5b0f4e5ddd029ecb
raditya-rizky <gusrizkydharmaputra123@gmail.com>
2022-08-26 14:46:26 +0800

    minor edit

--- config/config.js
+++ config/config.js
@@ -5,21 +5,27 @@ module.exports = {
     username: process.env.DB_USERNAME_DEV || "postgres",
     password: process.env.DB_PASSWORD_DEV || "postgres",
     database: process.env.DB_DEV || "ch9-challange_development",
-    host: "localhost",
+    host: "127.0.0.1",
     dialect: "postgres",
   },
   test: {
     username: process.env.DB_USERNAME_TEST || "postgres",
     password: process.env.DB_PASSWORD_TEST || "postgres",
     database: process.env.DB_TEST || "ch9-challange_test",
-    host: "localhost",
-    dialect: "postgres",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
   production: {
-    username: process.env.DB_USERNAME_DEV || "postgres",
-    password: process.env.DB_PASSWORD_DEV || "postgres",
-    database: process.env.DB_DEV || "ch9-challange_development",
-    host: "localhost",
+    // Reference: https://stackoverflow.com/a/70243144
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
+    protocol: "postgres",
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
