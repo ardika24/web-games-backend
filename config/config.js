1824e0d09b9259da3e3a56362df536f80ce703d6
raditya-rizky <gusrizkydharmaputra123@gmail.com>
2022-08-26 13:56:55 +0800

    minor edit

--- config/config.js
+++ config/config.js
@@ -16,10 +16,10 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: process.env.DB_USERNAME_PROD || "postgres",
-    password: process.env.DB_PASSWORD_PROD || "postgres",
-    database: process.env.DB_PROD || "ch9-challange_production",
-    host: "localhost",
+    username: process.env.DB_USERNAME_DEV || "postgres",
+    password: process.env.DB_PASSWORD_DEV || "postgres",
+    database: process.env.DB_DEV || "ch9-challange_development",
+    host: "127.0.0.1",
     dialect: "postgres",
   },
 };
