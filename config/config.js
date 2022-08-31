9ce138d6e22065e1415ca92ad55c93286310f5d1
ardika24 <ardhieka24@gmail.com>
2022-08-31 19:47:13 +0700

    fixing api.spec

--- config/config.js
+++ config/config.js
@@ -1,26 +1,26 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME_DEV || "postgres",
-    password: process.env.DB_PASSWORD_DEV || "postgres",
-    database: process.env.DB_DEV || "ch9-challange_development",
-    host: "localhost",
-    dialect: "postgres",
+    username: process.env.DB_USERNAME_DEV || 'postgres',
+    password: process.env.DB_PASSWORD_DEV || 'postgres',
+    database: process.env.DB_DEV || 'ch9-challange_development',
+    host: '127.0.0.1',
+    dialect: 'postgres',
   },
   test: {
-    username: process.env.DB_USERNAME_TEST || "postgres",
-    password: process.env.DB_PASSWORD_TEST || "postgres",
-    database: process.env.DB_TEST || "ch9-challange_test",
-    host: "localhost",
-    dialect: "postgres",
+    username: process.env.DB_USERNAME_TEST || 'postgres',
+    password: process.env.DB_PASSWORD_TEST || 'postgres',
+    database: process.env.DB_TEST || 'ch9-challange_test',
+    host: '127.0.0.1',
+    dialect: 'postgres',
     logging: false,
   },
   production: {
     // Reference: https://stackoverflow.com/a/70243144
-    use_env_variable: "DATABASE_URL",
-    dialect: "postgres",
-    protocol: "postgres",
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    protocol: 'postgres',
     ssl: true,
     dialectOptions: {
       ssl: {
--- test/api.spec.js
+++ test/api.spec.js
@@ -1,44 +1,48 @@
-const request = require("supertest");
-const app = require("../app");
+const request = require('supertest');
+const app = require('../app');
 
-describe("POST /api/v1/auth/register", () => {
-  const userPayload = { username: "test.superman", password: "superman123" };
+describe('POST /api/v1/auth/register', () => {
+  const userPayload = {
+    email: 'superman@email.com',
+    username: 'test.superman',
+    password: 'superman123',
+  };
 
   it('status: 400 and code "auth/register-invalid when request body empty', async () => {
     const response = await request(app)
-      .post("/api/v1/auth/register")
-      .send({ username: "", password: "" })
-      .set("Accept", "application.json");
+      .post('/api/v1/auth/register')
+      .send({ email: '', username: '', password: '' })
+      .set('Accept', 'application.json');
 
-    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.headers['content-type']).toMatch(/json/);
     expect(response.status).toEqual(400);
 
-    expect(response.body.error.code).toEqual("auth/register-invalid");
+    expect(response.body.error.code).toEqual('auth/register-invalid');
   });
 
-  it("status: 200 and the credential information", async () => {
+  it('status: 200 and the credential information', async () => {
     const response = await request(app)
-      .post("/api/v1/auth/register")
+      .post('/api/v1/auth/register')
       .send(userPayload)
-      .set("Accept", "application/json");
+      .set('Accept', 'application/json');
 
-    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.headers['content-type']).toMatch(/json/);
     expect(response.status).toEqual(200);
 
-    expect(response.body).toHaveProperty("id");
-    expect(response.body).toHaveProperty("accessToken");
+    expect(response.body).toHaveProperty('id');
+    expect(response.body).toHaveProperty('accessToken');
     expect(response.body.username).toEqual(userPayload.username);
   });
 
   it('status: 400 and code "auth/user-exist" when user already exist', async () => {
     const response = await request(app)
-      .post("/api/v1/auth/register")
+      .post('/api/v1/auth/register')
       .send(userPayload)
-      .set("Accept", "application/json");
+      .set('Accept', 'application/json');
 
-    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.headers['content-type']).toMatch(/json/);
     expect(response.status).toEqual(400);
 
-    expect(response.body.error.code).toEqual("auth/user-exists");
+    expect(response.body.error.code).toEqual('auth/user-exists');
   });
 });
