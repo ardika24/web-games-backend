c3aa9410a36bb129a0af8a43774e7717c2b9a2f9
ardika24 <ardhieka24@gmail.com>
2022-08-31 23:21:45 +0700

    add other test api and function

--- config/config.js
+++ config/config.js
@@ -2,17 +2,17 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME_DEV || 'postgres',
-    password: process.env.DB_PASSWORD_DEV || 'postgres',
-    database: process.env.DB_DEV || 'ch9-challange_development',
-    host: '127.0.0.1',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: `${process.env.DB_NAME}_development`,
+    host: 'localhost',
     dialect: 'postgres',
   },
   test: {
-    username: process.env.DB_USERNAME_TEST || 'postgres',
-    password: process.env.DB_PASSWORD_TEST || 'postgres',
-    database: process.env.DB_TEST || 'ch9-challange_test',
-    host: '127.0.0.1',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: `${process.env.DB_NAME}_test`,
+    host: 'localhost',
     dialect: 'postgres',
     logging: false,
   },
--- test/api.spec.js
+++ test/api.spec.js
@@ -43,6 +43,51 @@ describe('POST /api/v1/auth/register', () => {
     expect(response.headers['content-type']).toMatch(/json/);
     expect(response.status).toEqual(400);
 
-    expect(response.body.error.code).toEqual('auth/user-exists');
+    expect(response.body.error.code).toEqual('auth/user-exist');
+  });
+});
+
+describe('POST /api/v1/auth/login', () => {
+  const userPayload = {
+    username: 'test.superman',
+    password: 'superman123',
+  };
+
+  it('status: 400 and code "auth/login" when request body empty', async () => {
+    const response = await request(app)
+      .post('/api/v1/auth/login')
+      .send({ username: '', password: 'superman123' })
+      .set('Accept', 'application/json');
+
+    expect(response.headers['content-type']).toMatch(/json/);
+    expect(response.status).toEqual(400);
+
+    expect(response.body.error.message).toEqual('User not Found!');
+  });
+
+  it('status: 400 and code "auth/login" when request body empty', async () => {
+    const response = await request(app)
+      .post('/api/v1/auth/login')
+      .send({ username: 'test.superman', password: 'superman12345' })
+      .set('Accept', 'application/json');
+
+    expect(response.headers['content-type']).toMatch(/json/);
+    expect(response.status).toEqual(400);
+
+    expect(response.body.error.message).toEqual('Wrong password');
+  });
+
+  it('status: 200 and the credential information', async () => {
+    const response = await request(app)
+      .post('/api/v1/auth/login')
+      .send(userPayload)
+      .set('Accept', 'application/json');
+
+    expect(response.headers['content-type']).toMatch(/json/);
+    expect(response.status).toEqual(200);
+
+    expect(response.body).toHaveProperty('id');
+    expect(response.body).toHaveProperty('accessToken');
+    expect(response.body.username).toEqual(userPayload.username);
   });
 });
